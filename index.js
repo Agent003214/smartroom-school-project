e5a089a320586627b6c4eee6bd787a960a101135
Agent003214 <adamzsolt98@gmail.com>
2018-03-02 11:57:31 +0100

    Commit test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
  
 wss.on('connection', function connection(ws) {
-	console.log('connect ok');
+	console.log('connected');
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
