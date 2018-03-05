92db641784e5fbb95b263bd822257e683255a87c
Agent003214 <36987954+Agent003214@users.noreply.github.com>
2018-03-05 21:17:27 +0100

    Add files via upload
    Lámpa teszt kód

--- mqttconnector.js
+++ mqttconnector.js
@@ -13,3 +13,16 @@ client.on('message', function (topic, message) {
   console.log(topic.toString())
   //client.end()
 })
+
+//Semmi/teszt, szerintem így nem is mûködik
+client.on('connect', function () {
+    console.log('connect')
+    var lampa = client.subscribe('Neumann/SmartRoom/Livingroom/Ambient')
+    if (lampa==1) {
+        client.publish('Neumann/SmartRoom/Livingroom/Lamp/1', '0')
+    }
+    else if (lampa==0) {
+        client.publish('Neumann/SmartRoom/Livingroom/Lamp/1', '1')
+    }
+    
+})
