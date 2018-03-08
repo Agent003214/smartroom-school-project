812a83398a36cd467af6213abeeecbcf881b99fa
Agent003214 <36987954+Agent003214@users.noreply.github.com>
2018-03-08 01:22:03 +0100

    kiegészítés

--- mqttconnector.js
+++ mqttconnector.js
@@ -15,4 +15,71 @@ client.on('message', function (topic, message) {
   else if(topic=="Neumann/SmartRoom/Frontyard/Ambient"&&message==1){
         client.publish("Neumann/SmartRoom/Livingroom/Lamp/2","1");
   }
+
+    //2 szenzoros
+ /* if ((topic == "Neumann/SmartRoom/Frontyard/Ambient" && message == 0) && (topic == "Neumann/SmartRoom/Livingroom/Ambient" && message == 0)) {
+      client.publish("Neumann/SmartRoom/Livingroom/Lamp/1", "1");
+      client.publish("Neumann/SmartRoom/Livingroom/Lamp/2", "1");
+  }
+  else if ((topic == "Neumann/SmartRoom/Frontyard/Ambient" && message == 1) && (topic == "Neumann/SmartRoom/Livingroom/Ambient" && message == 0)) {
+      client.publish("Neumann/SmartRoom/Livingroom/Lamp/1", "0");
+      client.publish("Neumann/SmartRoom/Livingroom/Lamp/2", "0");
+  }*/
 })
+
+
+//Redõny
+client.on('connect', function () {
+    client.subscribe('Neumann/SmartRoom/Livingroom/Window')
+    client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    console.log(topic)
+    if (topic == 'Neumann/SmartRoom/Livingroom/Shades' && message >= 2) {
+        console.log("A redõny le van húzva")
+    }
+    else if (topic == 'Neumann/SmartRoom/Livingroom/Shades' && message <= 2) {
+        client.publish("Neumann/SmartRoom/Livingroom/Window")
+    }
+})
+
+//Tûz
+client.on('connect', function () {
+    client.subscribe('Neumann/SmartRoom/Livingroom/Smoke');
+})
+
+client.on('message', function (topic, message) {
+
+    console.log(message);
+    console.log(topic);
+    if (topic == "Neumann/SmartRoom/Livingroom/Smoke" && message == 0) {
+        break;
+    }
+    else if (topic == "Neumann/SmartRoom/Livingroom/Smoke" && message == 1) {
+        client.publish("Neumann/SmartRoom/Livingroom/Window", "0");
+        client.publish("Neumann/SmartRoom/Livingroom/Heater", "0");
+        client.publish("Neumann/SmartRoom/Livingroom/Cooler", "100");
+        client.publish("Neumann/SmartRoom/Frontyard/Doorlock", "0");
+        client.publish("Neumann/SmartRoom/Livingroom/Lamp/1", "0");
+        client.publish("Neumann/SmartRoom/Livingroom/Lamp/2", "0");
+    }
+})
+
+
+//alapkód
+/*var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+    client.subscribe('presence')
+    client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+})*/
